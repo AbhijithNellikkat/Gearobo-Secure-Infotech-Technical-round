895ffb33c636e04f168a2a5246c4861d5b2b8884
Abhijith Nellikkat <abhijithnellikkat@gmail.com>
2024-03-23 12:04:26 +0530

    sort , search done

--- lib/controllers/users_controller.dart
+++ lib/controllers/users_controller.dart
@@ -7,6 +7,8 @@ class UsersController extends ChangeNotifier {
   bool loading = false;
   List<dynamic> users = [];
 
+  List<dynamic> filteredUsers = [];
+
   final UsersService service = UsersService();
 
   Future<void> fetchAllUsers() async {
@@ -15,6 +17,7 @@ class UsersController extends ChangeNotifier {
       notifyListeners();
 
       users = await service.fetchAllUsers();
+      filteredUsers = List.from(users);
 
       loading = false;
       notifyListeners();
@@ -23,5 +26,30 @@ class UsersController extends ChangeNotifier {
       notifyListeners();
       log('Error from UsersController : $e');
     }
+  }
+
+  void sortUsersByEmail() {
+    users.sort((a, b) => a.email.compareTo(b.email));
+    filteredUsers.sort((a, b) => a.email.compareTo(b.email));
+    notifyListeners();
+  }
+
+  void sortUsersByName() {
+    users.sort((a, b) => a.name.compareTo(b.name));
+    filteredUsers.sort((a, b) => a.name.compareTo(b.name));
+    notifyListeners();
+  }
+
+  void searchUsers(String query) {
+    if (query.isEmpty) {
+      filteredUsers = List.from(users);
+    } else {
+      filteredUsers = users
+          .where((user) =>
+              user.name!.toLowerCase().contains(query.toLowerCase()) ||
+              user.email!.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    }
+    notifyListeners();
   }
 }
--- lib/views/users_view.dart
+++ lib/views/users_view.dart
@@ -3,17 +3,28 @@ import 'package:provider/provider.dart';
 import 'package:technical_round_app1/controllers/users_controller.dart';
 
 class UsersView extends StatefulWidget {
-  const UsersView({super.key});
+  const UsersView({Key? key}) : super(key: key);
 
   @override
   State<UsersView> createState() => _UsersViewState();
 }
 
 class _UsersViewState extends State<UsersView> {
+  late TextEditingController _searchController;
+
   @override
   void initState() {
-    Provider.of<UsersController>(context, listen: false).fetchAllUsers();
     super.initState();
+    _searchController = TextEditingController();
+    Provider.of<UsersController>(context, listen: false).fetchAllUsers();
+  }
+
+  void _sortByEmail() {
+    Provider.of<UsersController>(context, listen: false).sortUsersByEmail();
+  }
+
+  void _sortByName() {
+    Provider.of<UsersController>(context, listen: false).sortUsersByName();
   }
 
   @override
@@ -22,36 +33,73 @@ class _UsersViewState extends State<UsersView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.sort_by_alpha),
+            onPressed: _sortByName,
+            tooltip: 'Sort by Name',
+          ),
+          IconButton(
+            icon: Icon(Icons.email),
+            onPressed: _sortByEmail,
+            tooltip: 'Sort by Email',
+          ),
+        ],
       ),
-      body: provider.loading
-          ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: provider.users.length,
-                itemBuilder: (context, index) {
-                  final data = provider.users[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: Card(
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          child: Text("${data.id}"),
-                        ),
-                        title: Text(
-                          "${data.name}",
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          "${data.email}",
-                          style: const TextStyle(fontWeight: FontWeight.w300),
-                        ),
-                      ),
-                    ),
-                  );
-                },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: (value) {
+                provider.searchUsers(value);
+              },
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
               ),
             ),
+          ),
+          Expanded(
+            child: provider.loading
+                ? const Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    itemCount: provider.filteredUsers.length,
+                    itemBuilder: (context, index) {
+                      final data = provider.filteredUsers[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: Card(
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              child: Text("${data.id}"),
+                            ),
+                            title: Text(
+                              "${data.name}",
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              "${data.email}",
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.w300),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 }
